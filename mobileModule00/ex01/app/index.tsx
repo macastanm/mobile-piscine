534a7da88cc23e7bab277682ad1bb37ce60e162c
marialuiza <mluizacmarques@gmail.com>
2025-05-21 12:08:19 +0100

    ex00

---
+++ mobileModule00/ex01/app/index.tsx
@@ -0,0 +1,31 @@
+import {Button, Text, View, StyleSheet} from "react-native";
+import React from 'react';
+
+export default function Index() {
+    const handlePress = () => {
+        console.log('Button was pressed!');
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>A simple text</Text>
+            <View style={styles.buttonContainer}>
+                <Button title="Click me" onPress={handlePress} />
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    text: {
+        fontSize: 20,
+        marginBottom: 20,
+    },
+    buttonContainer: {
+        width: 150,
+    },
+});
