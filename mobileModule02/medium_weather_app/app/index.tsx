74cfdb0c5460fa43a349f1699a950f5411ab8b7f
marialuiza <mluizacmarques@gmail.com>
2025-05-27 14:14:30 +0100

    mod02

---
+++ mobileModule02/medium_weather_app/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native';
+import {useLocation} from "@/app/locationContext";
+
+export default function CurrentlyScreen() {
+    const { location } = useLocation();
+    return (
+        <View style={styles.container}>
+            <Text>Currently</Text>
+            <Text>{location}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
