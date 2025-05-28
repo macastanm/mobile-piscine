e1a07576e4924e523ddba0721dfdc2636ac26544
marialuiza <mluizacmarques@gmail.com>
2025-05-28 11:39:48 +0100

    ex00 done

--- mobileModule02/medium_weather_app/app/weekly.tsx
+++ mobileModule02/medium_weather_app/app/weekly.tsx
@@ -3,14 +3,26 @@ import {useLocation} from "@/app/locationContext";
 
 export default function WeeklyScreen() {
 	const { location } = useLocation();
+	const isError = location === 'Permission to access location was denied' || location === 'Error';
+
 	return (
 		<View style={styles.container}>
-			<Text>Weekly</Text>
-			<Text>{location}</Text>
+			{ isError ? (
+				<Text style={styles.errorText}>
+					Geolocation is not available, please enable it in your App settings
+				</Text>
+			) : (
+				<View style={{ alignItems: 'center' }}>
+					<Text style={styles.title}>Weekly</Text>
+					<Text>{location}</Text>
+				</View>
+			)}
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+	container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 16 },
+	title: { fontSize: 24, fontWeight: 'bold', marginBottom: 12 },
+	errorText: { color: 'red', fontSize: 16, textAlign: 'center' },
 });
