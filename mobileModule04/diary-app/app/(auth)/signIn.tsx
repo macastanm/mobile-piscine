f92dce9d11d11d18c135dfc7a3a2e7dc7f2480a5
marialuiza <mluizacmarques@gmail.com>
2025-08-12 16:36:05 +0100

    google sign in in progress

--- mobileModule04/diary-app/app/(auth)/signIn.tsx
+++ mobileModule04/diary-app/app/(auth)/signIn.tsx
@@ -5,7 +5,13 @@ import * as WebBrowser from "expo-web-browser";
 import { useAuthRequest, makeRedirectUri } from "expo-auth-session";
 import * as React from "react";
 import { createTokenWithCode } from "@/utils/createTokenWithCode";
-import { GithubAuthProvider, signInWithCredential } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  signInWithCredential,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
+import * as Google from "expo-auth-session/providers/google";
 import { auth } from "@/firebaseConfig";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -24,26 +30,50 @@ export default function SignIn() {
   const [request, response, promptAsync] = useAuthRequest(
     {
       clientId: process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID!,
-      scopes: [ "identity", "user:email" ],
+      scopes: ["identity", "user:email"],
       redirectUri: makeRedirectUri(),
       usePKCE: false,
     },
     discovery
   );
 
+  const [userInfo, setUserInfo] = React.useState();
+
+  const [requestGoogle, responseGoogle, promptAsyncGoogle] =
+    Google.useAuthRequest({
+      iosClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_IOS!,
+      androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_ANDROID!,
+      // scopes: [ "identity", "user:email" ],
+      // redirectUri: makeRedirectUri(),
+      // usePKCE: false,
+    });
+
   React.useEffect(() => {
     handleResponse();
   }, [response]);
 
-  async function handleResponse() {
+  React.useEffect(() => {
+    handleResponseGoogle();
+  }, [responseGoogle]);
 
+  async function handleResponseGoogle() {
+    if (responseGoogle?.type === "success") {
+      const { id_token } = responseGoogle.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      console.log(credential);
+      const dataUser = await signInWithCredential(auth, credential);
+      console.log(dataUser);
+    }
+  }
+
+  async function handleResponse() {
     if (response?.type === "success") {
       const { code } = response.params;
 
       const { token_type, scope, access_token } = await createTokenWithCode(
         code
       );
-      
+
       if (!access_token) return;
 
       const credential = GithubAuthProvider.credential(access_token);
@@ -53,7 +83,7 @@ export default function SignIn() {
 
   return (
     <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 32, fontWeight: "bold" }}>SignIn</Text>
+      <Text style={{ fontSize: 32, fontWeight: "bold" }}>Sign In</Text>
       <Icon.Button
         name="github"
         color={currentTheme === "dark" ? "white" : "black"}
@@ -65,6 +95,19 @@ export default function SignIn() {
       >
         <Text style={{ fontSize: 17, fontWeight: "500" }}>
           Sign In with Github
+        </Text>
+      </Icon.Button>
+      <Icon.Button
+        name="google"
+        color={currentTheme === "dark" ? "white" : "black"}
+        backgroundColor="transparent"
+        size={30}
+        onPress={() => {
+          promptAsyncGoogle();
+        }}
+      >
+        <Text style={{ fontSize: 17, fontWeight: "500" }}>
+          Sign In with Google
         </Text>
       </Icon.Button>
     </View>
